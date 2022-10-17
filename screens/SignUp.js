efa51f411a2c9a5d9fe8910217e4d8ddb1fbd672
Marcel Grund <marcel.grund@smail.th-koeln.de>
2022-10-17 15:36:23 +0200

    Add Button.js TextInput.js SignUp.js and Edited App.js Progress from the old Repository

---
+++ screens/SignUp.js
@@ -0,0 +1,87 @@
+import React, { useRef } from 'react';
+import { Text, View } from 'react-native';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+import Button from '../components/Button.js';
+import TextInput from '../components/TextInput.js';
+
+const LoginSchema = Yup.object().shape({
+  id: Yup.string()
+  .min(8, 'Too Short!')
+  .max(8,'Too Long!')
+  .required('Required'),
+  pin: Yup.string()
+    .min(6, 'Too Short!')
+    .max(6, 'Too Long!')
+    .required('Required')
+});
+
+export default function Login() {
+
+  const { handleChange, handleSubmit, handleBlur, values,  errors, touched } = useFormik({
+    validationSchema: LoginSchema,
+    initialValues: { id: '', pin: '' },
+    onSubmit: values => {
+      alert(`Id: ${values.id}, Pin: ${values.pin}`)
+      console.log();
+    }
+  });
+
+  const pin = useRef(null);
+
+  return (
+    
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <Text style={{ color: '#223e4b', fontSize: 40, fontWeight: 'bold', marginBottom: 16 }}>
+        DeKom
+      </Text>
+      <View style={{ paddingHorizontal: 32, marginBottom: 16, width: '100%' }}>
+        <TextInput
+          icon="user"
+          placeholder="Enter your ID"
+          autoCapitalize="none"
+          autoCompleteType="cc-number"
+          keyboardAppearance="dark"
+          returnKeyType="next"
+          returnKeyLabel="next"
+          onChangeText={handleChange('id')}
+          onBlur={handleBlur('id')}
+          error={errors.id}
+          touched={touched.id}
+          onSubmitEditing={() => pin.current?.focus()}
+        />
+      </View>
+      <View style={{ paddingHorizontal: 32, marginBottom: 16, width: '100%' }}>
+        <TextInput
+          icon="key"
+          placeholder="Enter your PIN"
+          secureTextEntry
+          autoCompleteType="password"
+          keyboardType="number-pad"
+          autoCapitalize="none"
+          keyboardAppearance="dark"
+          returnKeyType="go"
+          returnKeyLabel="go"
+          onChangeText={handleChange('pin')}
+          onBlur={handleBlur('pin')}
+          error={errors.pin}
+          touched={touched.pin}
+          ref={pin}
+          onSubmitEditing={() => handleSubmit()}
+        />
+
+        
+      </View>
+      <Button label='Sign Up' onPress={handleSubmit} />
+    </View>
+  );
+}
+
