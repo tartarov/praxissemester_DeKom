37f94e3b35799270656e88213a3cc57707791180
timokue <kue.tim.gt@gmail.com>
2022-11-24 15:38:00 +0100

    removed mainScreen from App.js to avoid merge conflicts

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MainScreen from './screens/MainScreen';
 
 export default function App() {
   return (
-    <MainScreen />
-   /* <View style={styles.container}>
+    <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View> */
+    </View>
   );
 }
 
